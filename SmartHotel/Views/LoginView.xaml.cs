b56b7a6f89a4b1923f450635f65e524afe404ffa
Nguyen Huy Tu <huytu405@gmail.com>
2018-04-12 22:19:40 +0700

    Initial Version

---
+++ SmartHotel/Views/LoginView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SmartHotel.Views
+{
+    public partial class LoginView : ContentPage
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+
+            var userName = txtUserName.Text;
+            var passWord = txtPassword.Text;
+
+            if(string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(passWord))
+            {
+                DisplayAlert("Alert", "You are not enterd username or password", "OK");                
+            }
+            else
+            {
+                DisplayAlert("Alert", "You are log in", "OK");  
+            }
+        }
+    }
+}
